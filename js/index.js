d990a88d5bd2a344e056250c5ffbc5b9682da4f6
ztw123458 <jon123458@qq.com>
2019-11-04 10:41:36 +0800

    e洁家政jquery上传代码

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+	$('.wrapper').load('./pages/highcharts.html');
+	$('.left ul li').click(function(){
+		$(this).addClass('current');
+		$(this).siblings().removeClass('current');
+		if($(this).text() == '顾客管理'){
+			$('.wrapper').load('./pages/customer.html');
+		}else if($(this).text() == '员工管理'){
+			$('.wrapper').load('./pages/waiter.html');
+		}else if($(this).text() == '订单管理'){
+			$('.wrapper').load('./pages/order.html');
+		}else if($(this).text() == '地址管理'){
+			$('.wrapper').load('./pages/address.html');
+		}else if($(this).text() == '产品管理'){
+			$('.wrapper').load('./pages/product.html');
+		}else if($(this).text() == '评论管理'){
+			$('.wrapper').load('./pages/comment.html');
+		}else if($(this).text() == '栏目管理'){
+			$('.wrapper').load('./pages/category.html');
+		}
+
+	})
+	$('#index').click(function(){
+		$('.wrapper').load('./pages/highcharts.html');
+	})
+})
